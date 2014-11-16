c0aff67fe55fb64532f1e605aa8b24c5f3949338
Max Chadwick <mpchadwick@gmail.com>
2014-11-15 22:27:23 -0500

    Update Icb template

--- Templates/Icb.php
+++ Templates/Icb.php
@@ -41,7 +41,7 @@ class Template_Icb extends AbstractTemplate
      * @var string
      * @access protected
      */
-    protected $htmlBlock = '/<!--- ### --->(.*?)<!--- ### --->/is';
+    protected $htmlBlock = '/Domain Information(.*?)are issued on a first come/is';
 
     /**
 	 * Blocks within the raw output of the whois
@@ -61,39 +61,40 @@ class Template_Icb extends AbstractTemplate
 	 * @access protected
 	 */
     protected $blockItems = array(
-            1 => array('/Organization Name:\n(?>[\x20\t]*)(.+)/i' => 'contacts:owner:organization', 
-                    '/Street:\n(?>[\x20\t]*)(.+)/i' => 'contacts:owner:address', 
-                    '/City:\n(?>[\x20\t]*)(.+)/i' => 'contacts:owner:city', 
-                    '/State:\n(?>[\x20\t]*)(.+)/i' => 'contacts:owner:state', 
-                    '/Postal Code:\n(?>[\x20\t]*)(.+)/i' => 'contacts:owner:zipcode', 
-                    '/Country:\n(?>[\x20\t]*)(.+)/i' => 'contacts:owner:country', 
-                    '/Created:\n(?>[\x20\t]*)(.+)/i' => 'created', 
-                    '/Last Updated:\n(?>[\x20\t]*)(.+)/i' => 'changed', 
-                    '/Expires:\n(?>[\x20\t]*)(.+)/i' => 'expires'), 
-            2 => array('/User ID:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:handle', 
-                    '/Contact Name:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:name', 
-                    '/Organization Name:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:organization', 
-                    '/Street:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:address', 
-                    '/City:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:city', 
-                    '/State:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:state', 
-                    '/Postal Code:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:zipcode', 
-                    '/Country:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:country', 
-                    '/Phone:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:phone', 
-                    '/Fax:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:fax', 
-                    '/E-Mail:\n(?>[\x20\t]*)(.+)/i' => 'contacts:admin:email'), 
-            3 => array('/User ID:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:handle', 
-                    '/Contact Name:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:name', 
-                    '/Organization Name:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:organization', 
-                    '/Street:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:address', 
-                    '/City:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:city', 
-                    '/State:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:state', 
-                    '/Postal Code:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:zipcode', 
-                    '/Country:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:country', 
-                    '/Phone:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:phone', 
-                    '/Fax:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:fax', 
-                    '/E-Mail:\n(?>[\x20\t]*)(.+)/i' => 'contacts:tech:email'), 
-            4 => array('/Nameserver:\n(?>[\x20\t]*)(.+)/i' => 'nameserver', 
-                    '/IP Address:\n(?>[\x20\t]*)(.+)/i' => 'ips'));
+            1 => array('/Organization Name : (?>[\x20\t]*)(.+)/i' => 'contacts:owner:organization',
+                    '/Street : (?>[\x20\t]*)(.+)/i' => 'contacts:owner:address',
+                    '/City : (?>[\x20\t]*)(.+)/i' => 'contacts:owner:city',
+                    '/State : (?>[\x20\t]*)(.+)/i' => 'contacts:owner:state',
+                    '/Postal Code : (?>[\x20\t]*)(.+)/i' => 'contacts:owner:zipcode',
+                    '/Country : (?>[\x20\t]*)(.+)/i' => 'contacts:owner:country',
+                    '/First Registered : (?>[\x20\t]*)(.+)/i' => 'created',
+                    '/Last Updated : (?>[\x20\t]*)(.+)/i' => 'changed',
+                    '/Domain Status : (?>[\x20\t]*)(.+)/i' => 'status',
+                    '/Expires : (?>[\x20\t]*)(.+)/i' => 'expires'),
+            2 => array('/User ID : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:handle',
+                    '/Contact Name : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:name',
+                    '/Organization Name : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:organization',
+                    '/Street : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:address',
+                    '/City : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:city',
+                    '/State : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:state',
+                    '/Postal Code : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:zipcode',
+                    '/Country : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:country',
+                    '/Phone : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:phone',
+                    '/Fax : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:fax',
+                    '/E-Mail : (?>[\x20\t]*)(.+)/i' => 'contacts:admin:email'),
+            3 => array('/User ID : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:handle',
+                    '/Contact Name : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:name',
+                    '/Organization Name : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:organization',
+                    '/Street : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:address',
+                    '/City : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:city',
+                    '/State : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:state',
+                    '/Postal Code : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:zipcode',
+                    '/Country : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:country',
+                    '/Phone : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:phone',
+                    '/Fax : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:fax',
+                    '/E-Mail : (?>[\x20\t]*)(.+)/i' => 'contacts:tech:email'),
+            4 => array('/Name Server : (?>[\x20\t]*)(.+)/i' => 'nameserver',
+                    '/IP Address : (?>[\x20\t]*)(.+)/i' => 'ips'));
 
     /**
      * RegEx to check availability of the domain name
@@ -101,5 +102,5 @@ class Template_Icb extends AbstractTemplate
      * @var string
      * @access protected
      */
-    protected $available = '/(There is no live registration)/i';
+    protected $available = '/available for purchase/i';
 }
