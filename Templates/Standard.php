a475da0647d1f923905e2502a72ba7839b5db5ad
AllenJB <AllenJB@users.noreply.github.com>
2015-02-16 09:29:33 +0000

    Additional whois servers
    Also added another rate limit string

--- Templates/Standard.php
+++ Templates/Standard.php
@@ -105,7 +105,7 @@ class Standard extends KeyValue
 
     protected $availabilityValues = array('available', 'not registered', 'free');
 
-    protected $rateLimit = '/(Quota Exceeded|exceeded the maximum allowable|exceeded your query limit|restricted due to excessive queries)|WHOIS LIMIT EXCEEDED|due to query limit controls/i';
+    protected $rateLimit = '/(Quota Exceeded|exceeded the maximum allowable|exceeded your query limit|restricted due to excessive queries)|WHOIS LIMIT EXCEEDED|due to query limit controls|You have exceeded you allotted number of/i';
 
 
     public function postProcess(&$WhoisParser)
