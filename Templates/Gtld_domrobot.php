5b9be386a643b8e1f0e85275ef3cf6d734371b93
Fernando Almeida <nanditu@gmail.com>
2017-03-17 21:47:49 +0000

    Added new Template\Gtld_domrobot to parse whois data correctly. Update Config/whois.ini to reference the new template.

--- Templates/Gtld_domrobot.php
+++ Templates/Gtld_domrobot.php
@@ -38,20 +38,20 @@ class Gtld_domrobot extends Regex
 {
 
     /**
-	 * Blocks within the raw output of the whois
-	 * 
-	 * @var array
-	 * @access protected
-	 */
+     * Blocks within the raw output of the whois
+     * 
+     * @var array
+     * @access protected
+     */
     protected $blocks;
 
 
     /**
-	 * Items for each block
-	 * 
-	 * @var array
-	 * @access protected
-	 */
+     * Items for each block
+     * 
+     * @var array
+     * @access protected
+     */
     protected $blockItems;
 
 
@@ -61,73 +61,74 @@ class Gtld_domrobot extends Regex
      * @var array
      * @access protected
      */
-	const CONTACTS = [
-		'admin' => 'admin',
-		'tech' => 'tech',
-		'billing' => 'billing',
-		'registrant' => 'registrant',
-	];
+    const CONTACTS = [
+        'admin' => 'admin',
+        'tech' => 'tech',
+        'billing' => 'billing',
+        'registrant' => 'registrant',
+    ];
 
 
 
-	public function __construct() {
-		$this->blocks = [
-			1 => '/^[\s\n\r]*[^:]*:[\s\n\r]*(.*)$/im',
+    public function __construct() {
+        $this->blocks = [
+            1 => '/^[\s\n\r]*[^:]*:[\s\n\r]*(.*)$/im',
             2 => '/\s*name\s*server\s*:(?>[\x20\t]*)(.*?)(?=dnssec)|$/is'
-		];
+        ];
 
-		$items = [];
-		/**
+        $items = [];
+        /**
          * Create contact specific regexps from base template
          */
-		foreach(self::CONTACTS as $regexp => $property) {
-			$items['/^\s*' . $regexp . '\s*name\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':name';
-    		$items['/^\s*' . $regexp . '\s*organization\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':organization';
-    		$items['/^\s*' . $regexp . '\s*street\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':street';
-    		$items['/^\s*' . $regexp . '\s*city\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':city';
-    		$items['/^\s*' . $regexp . '\s*state\/province\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':state';
-    		$items['/^\s*' . $regexp . '\s*postal\s*code\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':zipcode';
-    		$items['/^\s*' . $regexp . '\s*country\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':country';
-    		$items['/^\s*' . $regexp . '\s*phone\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':phone';
-    		$items['/^\s*' . $regexp . '\s*phone\s*ext\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':phone_ext';
-    		$items['/^\s*' . $regexp . '\s*fax\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':fax';
-    		$items['/^\s*' . $regexp . '\s*fax\s*ext\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':fax_ext';
-    		$items['/^\s*' . $regexp . '\s*email\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':email';
-    		$items['/^\s*registry\s*' . $regexp . '\s*id\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':id';
-    	}
+        foreach(self::CONTACTS as $regexp => $property) {
+            $items['/^\s*' . $regexp . '\s*name\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':name';
+            $items['/^\s*' . $regexp . '\s*organization\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':organization';
+            $items['/^\s*' . $regexp . '\s*street\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':street';
+            $items['/^\s*' . $regexp . '\s*city\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':city';
+            $items['/^\s*' . $regexp . '\s*state\/province\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':state';
+            $items['/^\s*' . $regexp . '\s*postal\s*code\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':zipcode';
+            $items['/^\s*' . $regexp . '\s*country\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':country';
+            $items['/^\s*' . $regexp . '\s*phone\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':phone';
+            $items['/^\s*' . $regexp . '\s*phone\s*ext\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':phone_ext';
+            $items['/^\s*' . $regexp . '\s*fax\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':fax';
+            $items['/^\s*' . $regexp . '\s*fax\s*ext\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':fax_ext';
+            $items['/^\s*' . $regexp . '\s*email\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':email';
+            $items['/^\s*registry\s*' . $regexp . '\s*id\s*:(?>[\x20\t]*)(.*?)$/im'] = 'contacts:' . $property . ':id';
+        }
 
-    	/**
-    	 * Domain data
-    	 */
-    	$items['/^\s*dnssec\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:dnssec';
-    	$items['/^\s*updated\s*date\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:updated_date';
-    	$items['/^\s*creation\s*date\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:creation_date';
-    	$items['/^\s*domain\s*name\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:name';
-    	$items['/^\s*domain\s*status\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:status';
-    	$items['/^\s*registry\s*domain\s*id\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:id';
-    	$items['/^\s*internationalized\s*domain\s*name\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:idna';
+        /**
+         * Domain data
+         */
+        $items['/^\s*dnssec\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:dnssec';
+        $items['/^\s*updated\s*date\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:updated_date';
+        $items['/^\s*creation\s*date\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:creation_date';
+        $items['/^\s*domain\s*name\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:name';
+        $items['/^\s*domain\s*status\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:status';
+        $items['/^\s*registry\s*domain\s*id\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:id';
+        $items['/^\s*internationalized\s*domain\s*name\s*:(?>[\x20\t]*)(.*?)$/im'] = 'domain:idna';
 
-    	/**
-    	 * Server data
-    	 */
-    	$items['/^\s*>>>\s*last\s*update\s*of\s*whois\s*database\s*:(?>[\x20\t]*)(.*?)\s*<<<\s*$/im'] = 'server:last_update';
+        /**
+         * Server data
+         */
+        $items['/^\s*>>>\s*last\s*update\s*of\s*whois\s*database\s*:(?>[\x20\t]*)(.*?)\s*<<<\s*$/im'] = 'server:last_update';
 
-    	/**
-    	 * Registrar data
-    	 */ 
-    	$items['/^\s*registrar\s*:(?>[\x20\t]*)(.*?)$/im'] = 'registrar:name';
-    	$items['/^\s*registrar\s*iana\s*id\s*:(?>[\x20\t]*)(.*?)$/im'] = 'registrar:iana_id';
-    	$items['/^\s*registrar\s*abuse\s*contact\s*email\s*:(?>[\x20\t]*)(.*?)$/im'] = 'registrar:abuse_email';
-    	$items['/^\s*registrar\s*abuse\s*contact\s*phone\s*:(?>[\x20\t]*)(.*?)$/im'] = 'registrar:abuse_phone';
-    	
-    	$this->blockItems = [
-    		1 => $items,
+        /**
+         * Registrar data
+         */ 
+        $items['/^\s*registrar\s*:(?>[\x20\t]*)(.*?)$/im'] = 'registrar:name';
+        $items['/^\s*registrar\s*iana\s*id\s*:(?>[\x20\t]*)(.*?)$/im'] = 'registrar:iana_id';
+        $items['/^\s*registrar\s*abuse\s*contact\s*email\s*:(?>[\x20\t]*)(.*?)$/im'] = 'registrar:abuse_email';
+        $items['/^\s*registrar\s*abuse\s*contact\s*phone\s*:(?>[\x20\t]*)(.*?)$/im'] = 'registrar:abuse_phone';
+        
+        $this->blockItems = [
+            1 => $items,
             2 => [
                 '/^\s*name\s*server\s*:(?>[\x20\t]*)(.*?)$/im' => 'domain:nameservers:nameserver'
-    	   ]
+           ]
         ];
-    	
-	}
+        
+    }
 
  
 }
+
